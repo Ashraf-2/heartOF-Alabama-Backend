d3796a0d8fa32139fe0864c2a74ad8e717b16970
Ashraf-2 <hasanashraful03@gmail.com>
2023-12-01 18:34:23 +0600

    updated all

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//food-campagin-A11
-//tvWaeIHIs0L7BQct
 
 // console.log(process.env.DB_USER)
 // console.log(process.env.DB_PASS)
@@ -34,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //create Database and collections
     const foodCampgaingDB = client.db("food-campagin-b8A11-DB");
@@ -223,7 +221,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
